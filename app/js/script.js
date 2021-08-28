d340b8aebe2c1447e192ccaff58c5071c1a8ca35
kingwell47 <joeldoctor47@gmail.com>
2021-08-28 10:18:23 +0800

    need to add data validation next

--- app/js/script.js
+++ app/js/script.js
@@ -6,13 +6,30 @@ const tipAmount = document.getElementById("tip-amount");
 const totalAmount = document.getElementById("tip-total");
 const btnReset = document.getElementById("btnReset");
 const calculatorForm = document.getElementById("tip-calculator");
+const errorMessage = document.getElementById("invalid-message")
 
 let tipObj = {}
 
 function displayTip(tipAmt, totalAmt) {
-    if (tipAmt > 0) {
-        tipAmount.value = tipAmt;
-        totalAmount.value = totalAmt;
+    if (tipObj.bill == 0) {
+        tipAmount.innerText = "$0.00";
+        totalAmount.innerText = "$0.00";
+    } else if (tipAmt > 0) {
+        tipAmount.innerText = "$" + tipAmt;
+        totalAmount.innerText = "$" + totalAmt;
+    }
+}
+
+function inputCheck(input) {
+    if (input.length > 20) {
+        return "";
+    }
+    return input.replace(/(-|\+)/, "");
+}
+
+function enableResetBtn() {
+    if (btnReset.disabled) {
+        btnReset.disabled = false;
     }
 }
 
@@ -20,7 +37,7 @@ function calculateTip(obj) {
     let tipPer = 0;
     let totalPer = 0;
     let totalTip = 0;
-    if (Object.keys(obj).length == 3) {
+    if (Object.keys(obj).length == 3 && obj.noOfPeople > 0) {
         totalTip = obj.bill * obj.tipPercent;
         tipPer = totalTip / obj.noOfPeople;
         totalPer = (parseFloat(obj.bill) + parseFloat(totalTip)) / obj.noOfPeople;
@@ -31,37 +48,56 @@ function calculateTip(obj) {
 }
 
 bill.addEventListener('input', event => {
-    btnReset.disabled = false;
+    
+    bill.value = inputCheck(bill.value); //resets if invalid input is detected
+    if (bill.value < 0) {
+        bill.value = Math.abs(bill.value); //prevents negative numbers
+    }    
+    enableResetBtn();
     tipObj.bill = bill.value;
     calculateTip(tipObj);
 })
 
 people.addEventListener('input', event => {
-    btnReset.disabled = false;
-    tipObj.noOfPeople = people.value;
-    calculateTip(tipObj);
+    if (!people.checkValidity() || people.value == 0) {
+        errorMessage.innerText = "Can't be that number"        
+    } else {
+        errorMessage.innerText = ""
+        enableResetBtn();
+        tipObj.noOfPeople = people.value;
+        calculateTip(tipObj);
+    }    
 })
 
 document.querySelectorAll('.selector__value').forEach(item => {
     item.addEventListener('change', event => {
-        btnReset.disabled = false;
+        enableResetBtn();        
         tipObj.tipPercent = item.value / 100;
         calculateTip(tipObj);
     })
   })
 
 customTip.addEventListener('input', event => {
-    btnReset.disabled = false;
+    customTip.value = inputCheck(customTip.value); //resets if invalid input detected
+    if (customTip.value < 0) {
+        customTip.value = Math.abs(customTip.value); //prevents negative numbers
+    }      
+    enableResetBtn();
     document.querySelectorAll('.selector__value').forEach(item => {
         item.checked = false;
     })
-    tipObj.tipPercent = customTip.value / 100;
-    calculateTip(tipObj);
+    if (customTip.value <= 100) { //prevents over 100%
+        tipObj.tipPercent = customTip.value / 100;
+        calculateTip(tipObj);
+    }
 })
 
 btnReset.addEventListener('click', event => {
-    calculatorForm.reset();
+    calculatorForm.reset();    
     tipObj = {}
+    tipAmount.innerText = "$0.00";
+    totalAmount.innerText = "$0.00";
+    errorMessage.innerText = ""
     btnReset.disabled = true;
 })
 
